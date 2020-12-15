b358618564c4a373721378dafe276cde7c7f2dba
rishit-khandelwal <rishitkh@gmail.com>
2020-12-15 10:04:11 +0530

    TODO finding implemented with trimming

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::{env, println};
+use std::{fs, ops::Index};
 
-fn getIgnore() -> Vec<String> {
-    use std::fs;
+fn get_ignores() -> Vec<String> {
+    let content: Vec<char> = fs::read(".gitignore")
+        .unwrap()
+        .into_iter()
+        .map(|byte| byte as char)
+        .collect();
 
-    let content: Vec<char> = fs::read(".gitignore").unwrap().into_iter().map(|byte| byte as char).collect();
     let mut ret_vec: Vec<String> = vec![String::from(".git")];
     let mut res = String::new();
 
@@ -19,31 +23,39 @@ fn getIgnore() -> Vec<String> {
     ret_vec
 }
 
-fn listFiles(dir: &str) -> std::io::Result<()> {
-    use std::fs;
-    
+fn list_files(dir: &str, files: &mut Vec<String>) -> std::io::Result<()> {
     let iter = fs::read_dir(dir)?;
 
     let iterator: Vec<fs::DirEntry> = iter.map(|r| r.unwrap()).collect::<Vec<fs::DirEntry>>();
 
-    for i in 0..(iterator.len()+1) {
+    for i in 0..(iterator.len() + 1) {
         let _dir: &fs::DirEntry = match iterator.get(i) {
             None => {
                 break;
-            },
-            Some(i) => i
+            }
+            Some(i) => i,
         };
         if _dir.file_type().unwrap().is_dir() {
-            match getIgnore().iter().find(|x| x == &&format!("{}/",_dir.file_name().into_string().unwrap())) {
+            match get_ignores()
+                .iter()
+                .find(|x| x == &&format!("{}/", _dir.file_name().into_string().unwrap()))
+            {
                 None => {
                     if _dir.file_name().into_string().unwrap() != ".git" {
-                        println!("{}/", _dir.file_name().into_string().unwrap());
+                        list_files(
+                            format!("./{}", _dir.file_name().into_string().unwrap()).as_str(),
+                            files,
+                        )?;
                     }
-                },
+                }
                 Some(_) => {}
             }
         } else {
-            println!("{}",_dir.file_name().into_string().unwrap());
+            files.push(format!(
+                "{}/{}",
+                dir,
+                _dir.file_name().into_string().unwrap()
+            ));
         }
     }
     Ok(())
@@ -56,8 +68,40 @@ fn main() {
     match argv[1].as_str() {
         "@for" => {
             if argv.len() == 3 {
-                let user  = argv[2].as_str();
-                listFiles(".");
+                let user = argv[2].as_str();
+                let mut filelist = vec![];
+                list_files(".", &mut filelist).unwrap();
+
+                for file in filelist {
+                    let name = file.as_str();
+                    let f = fs::read(name)
+                        .unwrap()
+                        .iter()
+                        .map(|x| *x as char)
+                        .collect::<Vec<char>>();
+                    let mut res = String::new();
+                    let mut lines: Vec<String> = vec![];
+                    let mut lno = 1;
+                    for i in f {
+                      if lines.len() != 0 {
+                        if lines.index(lno-2) == "// TODO" {
+                          println!("{} :{}", name, lno);
+                        }
+                      }
+                        if i == '\n' {
+                            lines.push(res);
+                            lno += 1;
+                            res = String::new();
+                            continue;
+                        }
+                        if (i == ' ' || i == '\t') && res == "" {
+                            continue;
+                        }
+                        // TODO
+
+                        res += &format!("{}", i);
+                    }
+                }
             }
         }
         _ => {}
